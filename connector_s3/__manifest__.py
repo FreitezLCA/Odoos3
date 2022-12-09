dd6446e994a7f05c188c5c7fdcaf2bdbedee4a7b
Cesar Freitez <cesar.freitez@chepss.cl>
2022-12-09 13:55:49 +0000

    [ADD] Creacion del modulo

---
+++ connector_s3/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Connector s3',
+    'sumary': """Módulo de sincronización de Odoo con S3""",
+    'description':"""
+        Sincronización de documentos entre odoo y AWS S3
+        - Carpetas
+        - Documentos
+    """,
+    'author': 'CHEPSS',
+    'website': 'https://www.chepss.cl',
+    'category': 'SINCRONIZACION',
+    'version': '0.1',
+    'depends': [],
+    'license': 'LGPL-3',
+    'application': True,
+    'sequence': 1,
+    'data':[
+        
+    ],
+    'demo':[
+        
+    ],
+}
